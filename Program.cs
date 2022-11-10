5bc91d694022a2c110ba2eba48482f2bca85a075
LeoGM <leograpperm@gmail.com>
2022-11-10 16:41:48 -0300

    realizada implementações comforme instruções do readme.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Nokia nokia = new Nokia("71931654531", "Nokia 6.1", "356926096671751", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Worms Armageddon");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone IPhone:");
+        Iphone iphone = new Iphone("95923925731", "APPLE iPhone 12 Pro Max", "356728118357207", 512);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Flappy Bird");
+    }
+}
